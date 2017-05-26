a25a2e5189b2e2c7fb0f5f51a16816b418b02e8d
Nikolaj Candellari <nikolajcandellari0@gmail.com>
2017-05-26 23:29:05 +0200

    zacel z tkinterjem, se veliko odprtih vprasanj

--- moj_minolovec/minsko_polje.py
+++ moj_minolovec/minsko_polje.py
@@ -3,47 +3,43 @@ import logika_minskega_polja
 
 class Minolovec:
 
+
     def __init__(self, zaslon):
-        self.polje = tk.Toplevel()
-        self.polje.pack()
+
+        self.stevec_min = tk.Label(zaslon, text = '10')
         
-        vrstica = []
-        for _ in range(9):
-            vrstica.append(0)
-        stolpec = []
-        for_ in range(9):
-            stolpec.append(0)
-        self.gumbi = tk.Button(row = , column = x)
-        self.gumbi.pack()
+        self.gumbi = range(81)
+        vrstice = []
+        for vrstice in range(9):
+            stolpci = []
+            for stolpci in range(9):
+                def klik_z_misko():
+                    if 'Button-1' == True: #ne najdem primerne funkcije, ki bi
+                                           # zaznala ko bi bil gumb kliknjen 
+                                           # z levo tipko na miski
+
+                        
+                        
+                    elif 'Button-2' == True: # isto za desno tipko
+                        self.stevec_min.config(text= str( int()))
+                self.gumbi = tk.Button(zaslon, height = 1, width = 1,
+                                       text = '', command = klik_z_misko )
+                self.gumbi.grid(row = vrstice, column = stolpci)
+                        
+       
+
+
+
+
         
+    def primerjaj_pritisnjen_gumb_s_tem_kar_je_v_matriki(self):
+        pass
 
+    def posodobi_stevec(self):
+        pass
 
-
-
-
-
-
-
-   
-zaslon = tk.Tk()
-self.gumbi.pack()
-zaslon.mainloop()
-
-
-
-
-
-
-
-'''naslov = tk.Label(zaslon, text ='MINOLOVEC')
-naslov.pack()
-
-def zapri():
-    zaslon.quit()
     
-gumb_za_izhod = tk.Button(zaslon, text = 'Izhod' , command = zapri)
-gumb_za_izhod.pack()
 
-gumb_za_zacetek_igre = tk.Button(zaslon, text = 'Nova igra'
-                                 , command = self.matrika)
-gumb_za_zacetek_igre.pack()'''
+zaslon = tk.Tk()
+Minolovec(zaslon)
+zaslon.mainloop()
